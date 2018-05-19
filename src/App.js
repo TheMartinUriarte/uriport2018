2fba21614bfc0e8411183ec135107d62f4a6556a
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-05-19 09:47:09 -0700

    [NEW] Created Header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Header from "./components/Header";
 import { Switch, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
@@ -9,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/businesscard" component={BizCard} />
